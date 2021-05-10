cbe6170258ba27d2bc01bfe45cfed10d0e3483cc
ysato <s171039@stn.nagaokaut.ac.jp>
2021-05-10 12:13:34 +0900

    バグの修正

--- eval.py
+++ eval.py
@@ -358,7 +358,7 @@ def non_conditional_eval():
 
     is_sufficient_size=lambda graph: True if graph.number_of_nodes()>size_th else False
 
-    vae = model.VAE(dfs_size, time_size, node_size, edge_size, model_param)
+    vae = model.VAENonConditional(dfs_size, time_size, node_size, edge_size, model_param)
     # vae.load_state_dict(torch.load("param/weight", map_location="cpu"))
     vae = utils.try_gpu(vae)
     vae.eval()
