656f5c3679fa495c494c9d47c29808f6b374d6b0
tyokoyama <s203193@stn.nagaokaut.ac.jp>
2021-10-07 16:29:06 +0900

    self_loss.pyを微修正

--- self_loss.py
+++ self_loss.py
@@ -7,6 +7,8 @@ class Encoder_Loss(nn.Module):
         super(Encoder_Loss, self).__init__()
 
     def forward(self, mu, sigma):
-        return -0.5 * (1+sigma-mu**2-torch.exp(sigma)).sum(axis=2).sum(axis=1)
-        # return -0.5 * torch.sum(1+sigma-mu**2-torch.exp(sigma))
+        # For train_seq_condition()
+        # return -0.5 * (1+sigma-mu**2-torch.exp(sigma)).sum(axis=2).sum(axis=1)
+        
+        return -0.5 * torch.sum(1+sigma-mu**2-torch.exp(sigma))
 
